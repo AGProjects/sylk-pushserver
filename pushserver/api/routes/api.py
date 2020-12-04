2a75c0930e5f6873f2cd1e2c39a45bd4a4248ed6
Tijmen de Mes <tijmen@ag-projects.com>
2020-12-04 12:19:11 +0000

    Enable v2 API routes

--- pushserver/api/routes/api.py
+++ pushserver/api/routes/api.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
 
 from pushserver.api.routes import home, push
+from pushserver.api.routes.v2 import add, push as push_v2, remove
+
 
 router = APIRouter()
 router.include_router(home.router, tags=["welcome", "home"])
 router.include_router(push.router, tags=["push"], prefix="/push")
+
+router.include_router(add.router, tags=["v2"], prefix="/v2/tokens")
+router.include_router(push_v2.router, tags=["v2"], prefix="/v2/tokens")
+router.include_router(remove.router, tags=["v2"], prefix="/v2/tokens")
+
