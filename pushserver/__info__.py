ab2c7dbddb76c7c6ba00eac617e59525f61478c0
Adrian Georgescu <ag@ag-projects.com>
2020-12-12 11:36:33 +0000

    Increased versdion and added changelog for version 2.0

--- pushserver/__info__.py
+++ pushserver/__info__.py
@@ -1,9 +1,9 @@
 """Package information"""
 
-__version__ = '1.0.7'
+__version__ = '2.0.0'
 __project__ = 'sylk_pushserver'
 __summary__ = 'Mobile push notifications for RTC infrastructures'
-__webpage__ = 'http://sylkserver.com'
+__webpage__ = 'https://sylkserver.com'
 __author__ = 'Bibiana Rivadeneira'
 __email__ = 'support@ag-projects.com'
 __license__ = 'GPL v3'
