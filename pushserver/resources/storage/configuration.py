3240daa7f3cbf7ad334f1e2e2b604a041423aa02
Tijmen de Mes <tijmen@ag-projects.com>
2020-12-16 12:57:47 +0000

    Set default log level for cassandra to info and add debug option from config

--- pushserver/resources/storage/configuration.py
+++ pushserver/resources/storage/configuration.py
@@ -48,6 +48,7 @@ class CassandraConfig(ConfigSection):
     cluster_contact_points = ConfigSetting(type=HostnameList, value=None)
     keyspace = ConfigSetting(type=str, value='')
     table = ConfigSetting(type=str, value='')
+    debug = False
 
 class ServerConfig(ConfigSection):
     __cfgfile__ = 'general.ini'
