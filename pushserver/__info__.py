e22059dee129546dc1350351836d9e47467b3ebc
Tijmen de Mes <tijmen@ag-projects.com>
2025-06-13 12:24:41 +0000

    Raised version and updated changelog

--- pushserver/__info__.py
+++ pushserver/__info__.py
@@ -1,10 +1,10 @@
 """Package information"""
 
-__version__ = '2.0.1'
+__version__ = '2.1.0'
 __project__ = 'sylk_pushserver'
 __summary__ = 'Mobile push notifications for RTC infrastructures'
 __webpage__ = 'https://sylkserver.com'
 __author__ = 'Bibiana Rivadeneira'
 __email__ = 'support@ag-projects.com'
 __license__ = 'GPL v3'
-__copyright__ = 'Copyright 2022 AG Projects'
+__copyright__ = 'Copyright 2025 AG Projects'
